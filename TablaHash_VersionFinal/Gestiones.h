0b08ac2c42efac32d47646d3855c38ebc922b716
ricpvt <158591406+ricpvt@users.noreply.github.com>
2024-06-03 04:27:33 -0700

    Delete TablaHash_VersionFinal directory

--- TablaHash_VersionFinal/Gestiones.h
+++
@@ -1,9 +0,0 @@
-#ifndef GESTIONES_H_INCLUDED
-#define GESTIONES_H_INCLUDED
-
-//*****************//Indice para tipo de datos enteros//*******************************************************************************************************************************************
-int IndiceEnt(const int & clave);
-//*****************//Indice para tipo de datos string//*******************************************************************************************************************************************
-int IndiceCad(const string& clave);
-
-#endif // GESTIONES_H_INCLUDED
